ad0320237bb78117d02c21fe20d6327bc9a1c04d
Andrei <andrei@Andreis-MacBook-Pro.local>
2023-11-19 23:14:47 +0100

    fix: buzz

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,18 @@ use std::time::Duration;
 fn main() {
     // Tell the Pi which GPIO pin you are using
     let mut buzzer = Buzzer::new(17);
+    loop {
+        // Make the buzzer switch on
+        buzzer.on();
+
+        // Wait for one second
+        sleep(Duration::from_secs(1));
+
+        // Make the buzzer switch off
+        buzzer.off();
+
+        // Wait for one second
+        sleep(Duration::from_secs(1));
+    }
+    println!("Ciao")
 }
